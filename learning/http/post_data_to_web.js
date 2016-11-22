f2d193f7d8fc29c95b926e07ec7236df584301a7
Nazmul Basher <nazmul.basher@fieldnation.com>
2016-11-22 17:16:05 +0600

    Changed file names

---
+++ learning/http/post_data_to_web.js
@@ -0,0 +1,20 @@
+var http = require('http');
+var options = {
+	host: "www.google.com.bd",
+	port: 80,
+	path: "/",
+	method: "POST"
+};
+
+var request = http.request(options, function(response) {
+	console.log('STATUS:', response.statusCode);
+	console.log('HEADERS:', response.headers);
+	response.setEncoding('utf8');
+	response.on('data', function (chunk) {
+		console.log('BODY:', chunk);
+	});
+});
+
+request.write('This is a piece of data.\n');
+request.write('This is another piece of data.\n');
+request.end();

43364d01425fc409e86c4e00c2155bc5c6e595fb
nazmulbasher <nazmul.basher@fieldnation.com>
2020-08-04 16:10:40 +0300

    Merge branch 'master' of https://github.com/nazmulb/node.js

---
+++ projects/microapp/api/src/models/user.js
@@ -0,0 +1,51 @@
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  User.init(
+    {
+      firstName: {
+        allowNull: false,
+        type: DataTypes.STRING,
+        validate: {
+          notNull: {
+            msg: "Please enter your first name",
+          },
+        },
+      },
+      lastName: DataTypes.STRING,
+      email: {
+        allowNull: false,
+        unique: true,
+        type: DataTypes.STRING,
+        validate: {
+          isEmail: {
+            msg: "Please enter a valid email address",
+          },
+        },
+      },
+      password: {
+        allowNull: false,
+        type: DataTypes.STRING,
+        validate: {
+          notNull: {
+            msg: "Please enter your password",
+          },
+        },
+      },
+    },
+    {
+      sequelize,
+      modelName: "User",
+    }
+  );
+  return User;
+};
